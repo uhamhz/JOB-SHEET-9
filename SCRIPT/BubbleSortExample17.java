3e17088a4e8caff2051f62a0cb6b96a4796e504d
uhamhz <ammarhafizh2404@gmail.com>
2023-10-24 20:31:18 +0700

    Change to Descending

--- SCRIPT/BubbleSortExample17.java
+++ SCRIPT/BubbleSortExample17.java
@@ -7,8 +7,8 @@ public class BubbleSortExample17 {
         int temp = 0;
 
         for (int i = 0; i < intData.length; i++) {
-            for (int j = 1; j < intData.length; j++) {
-                if (intData[j - 1] > intData[j]) {
+            for (int j = 1; j < intData.length-i; j++) {
+                if (intData[j - 1] < intData[j]) {
                     // swap elemen
                     temp = intData[j];
                     intData[j] = intData[j - 1];
