62a673c50ea81c3513f3d00430df82ce0613cd64
uhamhz <ammarhafizh2404@gmail.com>
2023-10-24 11:26:40 +0700

    Add New Function

--- SCRIPT/ArrayRataNilai17.java
+++ SCRIPT/ArrayRataNilai17.java
@@ -6,20 +6,33 @@ public class ArrayRataNilai17 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+        System.out.print("Masukan Jumlah Mahasiswa : ");
+        int length = input.nextInt();
+
+        int[] nilaiMhs = new int[length];
+        double total = 0, totalTidakLulus = 0;
+        double rata2, rataTidakLulus;
+        int counter = 0, counter2 = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
-            System.out.print("Masukan nilai mahasiswa ke-" + (i+1)+" : ");
+            System.out.print("Masukan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = input.nextInt();
         }
 
         for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                counter++;
+                total += nilaiMhs[i];
+            } else {
+                totalTidakLulus += nilaiMhs[i];
+                counter2++;
+            }
         }
 
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-Rata Nilai = "+rata2);
+        System.out.println("Jumlah Mahasiswa yang Lulus Adalah : " + counter);
+        rata2 = total / counter;
+        System.out.println("Rata-Rata Nilai Lulus = " + rata2);
+        rataTidakLulus = totalTidakLulus / counter2;
+        System.out.println("Rata-rata nilai tidak lulus = " + rataTidakLulus);
     }
 }
