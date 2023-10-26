aea290c6a47abbaa4f6386bbf8003a49b055028c
uhamhz <ammarhafizh2404@gmail.com>
2023-10-26 23:09:51 +0700

    Add New Files

---
+++ SCRIPT/LinearSearch17.java
@@ -0,0 +1,31 @@
+package SCRIPT;
+
+import java.util.Scanner;
+
+public class LinearSearch17 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukan Jumlah Elemen Array : ");
+        int length = input.nextInt();
+        int[] arrayInt = new int[length];
+        int key;
+        int hasil = 0;
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukan Nilai Elemen Ke-" + (i + 1) + " : ");
+            arrayInt[i] = input.nextInt();
+        }
+
+        System.out.print("Masukan Nilai yang Dicari : ");
+        key = input.nextInt();
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] == key) {
+                hasil = i;
+                break;
+            }
+        }
+        System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
+    }
+}
